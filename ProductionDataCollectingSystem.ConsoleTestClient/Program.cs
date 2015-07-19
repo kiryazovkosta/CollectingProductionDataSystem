5d71d46aa7beaeb2372012d573ffbc9fa68752ec
kosta.kiryazov@gmail.com <kosta.kiryazov@gmail.com>
2015-07-19 09:46:06 +0300

    qw

--- ProductionDataCollectingSystem.ConsoleTestClient/Program.cs
+++ ProductionDataCollectingSystem.ConsoleTestClient/Program.cs
@@ -10,11 +10,9 @@ namespace ProductionDataCollectingSystem.ConsoleTestClient
     {
         static void Main(string[] args)
         {
-<<<<<<< HEAD
-            Console.WriteLine("Hello from user 1");
-=======
             Console.WriteLine("Hello from user 2");
->>>>>>> 986541ef7cc2fd13fba5fd83225f2ff8232ac44f
+            Console.WriteLine("Hello from user 2");
+            Console.WriteLine("Hello from user 2");
         }
     }
 }
