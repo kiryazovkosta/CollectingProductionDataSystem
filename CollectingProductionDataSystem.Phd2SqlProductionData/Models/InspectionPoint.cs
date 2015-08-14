cc2222a05a8fad72469eed2bca836aeeef4015df
kiryazovkosta <kosta.kiryazov@gmail.com>
2015-08-14 11:29:32 +0300

    Added exterrnal library for communication with Uniformance PHD server

---
+++ CollectingProductionDataSystem.Phd2SqlProductionData/Models/InspectionPoint.cs
@@ -0,0 +1,12 @@
+﻿namespace CollectingProductionDataSystem.Phd2SqlProductionData.Models
+{
+
+    public class InspectionPoint
+    {
+        public int Id { get; set; }
+
+        public decimal Value { get; set; }
+
+        public string InspectionPointTag { get; set; }
+    }
+}
---
+++ CollectingProductionDataSystem.Phd2SqlProductionData/Utility.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollectingProductionDataSystem.Phd2SqlProductionData
+{
+    internal class Utility
+    {
+        public static void SetRegionalSettings()
+        {
+	        if (Properties.Settings.Default.FORCE_REGIONAL_SETTINGS) {
+		        CultureInfo newCi = new CultureInfo(Properties.Settings.Default.CULTURE_INFO, false);
+		        newCi.NumberFormat.CurrencyDecimalSeparator = ".";
+		        newCi.NumberFormat.CurrencyGroupSeparator = string.Empty;
+		        newCi.NumberFormat.NumberDecimalSeparator = ".";
+		        newCi.NumberFormat.NumberGroupSeparator = string.Empty;
+		        newCi.DateTimeFormat.ShortDatePattern = "dd.MM.yyyy";
+		        newCi.DateTimeFormat.ShortTimePattern = "HH:mm:ss";
+		        newCi.DateTimeFormat.LongTimePattern = "HH:mm:ss";
+		        newCi.DateTimeFormat.LongDatePattern = "dd.MM.yyyy";
+		        System.Threading.Thread.CurrentThread.CurrentCulture = newCi;
+	        }
+        }
+
+    }
+}
