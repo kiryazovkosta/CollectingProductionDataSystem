22e142be8c7a499fe09896ea24dec875c723ad24
niknhattan <nikhattan@yahoo.com>
2015-08-28 17:50:52 +0300

    side bar

--- Source/Web/CollectingProductionDataSystem.Web/Scripts/custom/sidebar.js
+++ Source/Web/CollectingProductionDataSystem.Web/Scripts/custom/sidebar.js
@@ -1,14 +1,22 @@
 ﻿$(document).ready(function () {
-    var sidebar = $("a#naw-collapse");
-    if (sidebar) {
-        sidebar.click(function () {
-            if ($(this).attr("aria-expanded") == "true") {
-                $(this).attr("aria-expanded", "false");
-                $("a#naw-collapse span").attr("class", "glyphicon glyphicon-chevron-down");
-            } else {
-                $(this).attr("aria-expanded", "true");
-                $("a#naw-collapse span").attr("class", "glyphicon glyphicon-chevron-up");
-            }
+    var sidebarContent = $('div.sidebar');
+    if (sidebarContent) {
+        $("a#slide").click(function () {
+            $("div#content").attr("style", "display:none;");
+            var slideButton = $(this);
+            sidebarContent.animate({ width: 'toggle' }, function () {
+                if (slideButton.attr("aria-expanded") == "true") {
+                    slideButton.attr("aria-expanded", "false");
+                    $("a#slide span").attr("class", "glyphicon glyphicon-chevron-down");
+                    //$("div#aside").attr("style", "max-width:20px;");
+                    $("div#content").attr("style", "width:99%; margin-left:30px;");
+                } else {
+                    slideButton.attr("aria-expanded", "true");
+                    $("a#slide span").attr("class", "glyphicon glyphicon-chevron-up");
+                    $("div#aside").attr("style", "");
+                    $("div#content").attr("style", "");
+                }
+            });
         });
     }
 });
