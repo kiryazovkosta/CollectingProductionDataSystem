2bf31def0d99605ff9c0bf5b8ab0f3ba7dd51750
niknhattan <nikhattan@yahoo.com>
2015-08-28 11:51:34 +0300

    SideBar evolution

---
+++ Source/Web/CollectingProductionDataSystem.Web/Scripts/custom/sidebar.js
@@ -0,0 +1,14 @@
+﻿$(document).ready(function () {
+    var sidebar = $("a#naw-collapse");
+    if (sidebar) {
+        sidebar.click(function () {
+            if ($(this).attr("aria-expanded") == "true") {
+                $(this).attr("aria-expanded", "false");
+                $("a#naw-collapse span").attr("class", "glyphicon glyphicon-chevron-down");
+            } else {
+                $(this).attr("aria-expanded", "true");
+                $("a#naw-collapse span").attr("class", "glyphicon glyphicon-chevron-up");
+            }
+        });
+    }
+});
