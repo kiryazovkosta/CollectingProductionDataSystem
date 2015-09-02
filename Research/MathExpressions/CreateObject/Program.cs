c362e10abaea422169df054aecd01e99086b7cb9
niknhattan <nikhattan@yahoo.com>
2015-09-02 11:36:08 +0300

    Entalpia Formula ready

--- Research/MathExpressions/CreateObject/Program.cs
+++ Research/MathExpressions/CreateObject/Program.cs
@@ -1,23 +1,41 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using ExpressionEvaluator;
 using MathExpressions.Application;
 
 namespace CreateObject
 {
     class Program
     {
-
         static void Main(string[] args)
+        {
+            CalculatorDemo();
+            PartialPrint(455, 20.4);
+        }
+
+        private static void CalculatorDemo()
         {
             var calculator = new Calculator();
             var inputParams = new Dictionary<string, double>();
             inputParams.Add("a", 2);
             inputParams.Add("b", 8);
-            Console.WriteLine(calculator.Calculate("Math.Pow(par.a ,par.b)", "par", 2, inputParams)); 
+            Console.WriteLine(calculator.Calculate("Math.Pow(par.a ,par.b)", "par", 2, inputParams));
+        }
+
+        /// <summary>
+        /// Partials the print.
+        /// </summary>
+        /// <param name="p">The p.</param>
+        /// <param name="p1">The p1.</param>
+        private static void PartialPrint(double t, double P)
+        {
+            var fullResult =(((0.758195 - (7.97826 / Math.Pow((0.001 * (t + 273.15)), 2)) - ((3.078455 * 0.001 * (t + 273.15) - 0.21549) / Math.Pow(0.001 * (t + 273.15) - 0.21, 3))) * 0.01 * P)+
+                            (0.0644126 - (0.268671 / Math.Pow(0.001 * (t + 273.15), 8)) - (0.216661 / Math.Pow(0.001 * (t + 273.15), 14) / 100)) * Math.Pow(0.01 * P, 2)+
+                            (503.43 + (11.02849 * Math.Log(((t + 273.15) / 647.6))) + (229.2569 * ((t + 273.15)/647.6)) + (37.93129 * Math.Pow(((t + 273.15) / 647.6), 2))))*0.001;
+
+            Console.WriteLine("Entalpia -> {0}", fullResult);
+
+
         }
     }
-
- 
 }
---
+++ Research/MathExpressions/MathExpressions.Tests/FormulasTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using MathExpressions.Application;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MathExpressions.Tests
+{
+    [TestClass]
+    public class FormulasTests
+    {
+        Calculator calculator = new Calculator();
+
+        [TestMethod]
+        public void EntalpiaFormulaTest()
+        {
+            //Arrange
+            double t = 455;
+            double P = 20.4;
+            double s = 1;
+
+            var inputParams = new Dictionary<string, double>();
+            inputParams.Add("t", t);
+            inputParams.Add("P", P);
+            double expected = 0.804416784655262;
+
+            string expr = @"(((0.758195 - (7.97826 / Math.Pow((0.001 * (par.t + 273.15)), 2)) - ((3.078455 * 0.001 * (par.t + 273.15) - 0.21549) / Math.Pow(0.001 * (par.t + 273.15) - 0.21, 3))) * 0.01 * par.P)+
+                            (0.0644126 - (0.268671 / Math.Pow(0.001 * (par.t + 273.15), 8)) - (0.216661 / Math.Pow(0.001 * (par.t + 273.15), 14) / 100)) * Math.Pow(0.01 * par.P, 2)+
+                            (503.43 + (11.02849 * Math.Log(((par.t + 273.15) / 647.6))) + (229.2569 * ((par.t + 273.15)/647.6)) + (37.93129 * Math.Pow(((par.t + 273.15) / 647.6), 2))))*0.001";
+            //Act
+            double actual = calculator.Calculate(expr,"par",2,inputParams);
+            //Assert
+            //.8044167846552613
+            Assert.AreEqual(Convert.ToDecimal(expected),Convert.ToDecimal(actual));
+        }
+
+    }
+}
