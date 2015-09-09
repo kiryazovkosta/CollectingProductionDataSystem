04bb05202d3983a8d17a1cda0569660b69c3a42b
niknhattan <nikhattan@yahoo.com>
2015-09-09 13:08:03 +0300

    Fixed EditReasonInputModel.cshtml

---
+++ Source/Web/CollectingProductionDataSystem.Web/Areas/ShiftReporting/Controllers/AjaxController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CollectingProductionDataSystem.Data.Contracts;
+
+namespace CollectingProductionDataSystem.Web.Areas.ShiftReporting.Controllers
+{
+    public class AjaxController : AreaBaseController
+    {
+        public AjaxController(IProductionData productionDataParam)
+            :base(productionDataParam)
+        { }
+    }
+}
