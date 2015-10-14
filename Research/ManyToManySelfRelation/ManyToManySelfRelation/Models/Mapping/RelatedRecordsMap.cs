1076e275acfcbc5ad23d8faaf60263353ae8a4f1
niknhattan <nikhattan@yahoo.com>
2015-10-14 17:59:14 +0300

    ManyToMany CodeFirst Self Relation

---
+++ Research/ManyToManySelfRelation/ManyToManySelfRelation/Models/Mapping/RelatedRecordsMap.cs
@@ -0,0 +1,23 @@
+﻿/// <summary>
+/// Summary description for RelatedRecordsMap
+/// </summary>
+namespace ManyToManySelfRelation.Models.Mapping
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Data.Entity.ModelConfiguration;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+
+    class RelatedRecordsMap : EntityTypeConfiguration<RelatedRecords>
+    {
+        public RelatedRecordsMap()
+        {
+            this.HasKey(t => new { t.RecordId, t.RelatedRecordId });
+
+            this.HasRequired(p => p.RelatedRecord).WithMany().WillCascadeOnDelete(false);
+        }
+    }
+}
