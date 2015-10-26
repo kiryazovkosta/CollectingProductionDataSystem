2657501b52ca11d6e275aa1ea9eab7546fb0d553
kiryazovkosta <kosta.kiryazov@gmail.com>
2015-10-26 13:30:24 +0200

    Base primary data service for read and save

--- Source/Web/CollectingProductionDataSystem.Web/Areas/ShiftReporting/Controllers/UnitsController.cs
+++ Source/Web/CollectingProductionDataSystem.Web/Areas/ShiftReporting/Controllers/UnitsController.cs
@@ -162,7 +162,10 @@
                         status = data.SaveChanges(this.UserProfile.UserName);
                         if (status.IsValid)
 	                    {
-                            var approvedShiftsCount = this.data.UnitsApprovedDatas.All().Where(x => x.ProcessUnitId == model.processUnitId.Value && x.RecordDate == model.date.Value).Count();
+                            var approvedShiftsCount = this.data.UnitsApprovedDatas
+                                .All()
+                                .Where(x => x.ProcessUnitId == model.processUnitId.Value && x.RecordDate == model.date.Value)
+                                .Count();
                             var shiftsCount = this.data.Shifts.All().Count();
                             if (approvedShiftsCount == shiftsCount)
                             {
