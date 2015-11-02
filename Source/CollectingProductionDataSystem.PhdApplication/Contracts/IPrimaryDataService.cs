0a6dce076e891941857b1e8052a52231ad8aa82f
kiryazovkosta <kosta.kiryazov@gmail.com>
2015-11-02 13:02:38 +0200

    Added selecting primary data for past shifts functionality

--- Source/CollectingProductionDataSystem.PhdApplication/Contracts/IPrimaryDataService.cs
+++ Source/CollectingProductionDataSystem.PhdApplication/Contracts/IPrimaryDataService.cs
@@ -1,14 +1,10 @@
 ﻿namespace CollectingProductionDataSystem.PhdApplication.Contracts
 {
-    using CollectingProductionDataSystem.Data.Contracts;
     using System;
-    using System.Collections.Generic;
     using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
 
     public interface IPrimaryDataService
     {
-        int ReadAndSaveUnitsDataForShift();
+        int ReadAndSaveUnitsDataForShift(DateTime beginDateTime, int offsetInHours);
     }
 }
