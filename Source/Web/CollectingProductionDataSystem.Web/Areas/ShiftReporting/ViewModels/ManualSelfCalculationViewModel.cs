2df443e3ee645738d61395440cdd7a85a9c4e5af
kiryazovkosta <kosta.kiryazov@gmail.com>
2015-11-05 12:48:05 +0200

    Added SelfCalculateManualEntry

---
+++ Source/Web/CollectingProductionDataSystem.Web/Areas/ShiftReporting/ViewModels/ManualSelfCalculationViewModel.cs
@@ -0,0 +1,27 @@
+﻿namespace CollectingProductionDataSystem.Web.Areas.ShiftReporting.ViewModels
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.Linq;
+    using System.Web;
+    using Resources = App_GlobalResources.Resources;
+
+    public class ManualSelfCalculationViewModel
+    {
+        [Required(ErrorMessageResourceName = "Required", ErrorMessageResourceType = typeof(Resources.ErrorMessages))]
+        [Range(0, double.MaxValue, ErrorMessageResourceName = "ManualValue", ErrorMessageResourceType = typeof(Resources.ErrorMessages))]
+        [Display(Name = "NewValue", ResourceType = typeof(Resources.Layout))]
+        public decimal Value { get; set; }
+
+        [Required]
+        [UIHint("Hidden")]
+        public int UnitDataId { get; set; }
+
+        public string EditorScreenHeading { get; set; }
+
+        [Required]
+        [UIHint("Hidden")]
+        public string MeasurementCode{ get; set; }
+    }
+}
