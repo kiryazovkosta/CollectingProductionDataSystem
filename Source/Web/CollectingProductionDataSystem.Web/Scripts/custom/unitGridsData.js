93eee501da500b8d7f22ea734bd9b814a3702997
niknhattan <nikhattan@yahoo.com>
2015-11-06 11:01:49 +0200

    Consolidation of JavaScript

--- Source/Web/CollectingProductionDataSystem.Web/Scripts/custom/unitGridsData.js
+++ Source/Web/CollectingProductionDataSystem.Web/Scripts/custom/unitGridsData.js
@@ -1,34 +1,17 @@
 var unitGridsData = (function () {
-    //local
-    var sendProcessUnit = function () {
-        return { "processUnitId": $('input[name=processunits]').val() };
-    }
 
-    //local
-    var sendShift = function sendShift() {
-        return { "shiftId": $('input[name=shifts]').val() }
-    }
-
-    //global
-    var sendDate = function () {
-        var result = { "date": $('input[name=date]').val() };
-        $.extend(result, sendProcessUnit());
-        if ($('input[name=shifts]')) {
-            $.extend(result, sendShift());
-        }
-        $.extend(result, sendAntiForgery());
-        return result;
-    }
-
-    //function recordTimeStampFilter(element) {
-    //    element.kendoDateTimePicker();
-    //}
-
-    //local
+    // ----------------- autorun function on document ready -------------------
     $(document).ready(function () {
         $("#apply").click(function () {
-            kendoAdditional.RefreshGrid("#units");
-            kendoAdditional.RefreshGrid("#productionPlan");
+            if ($("#units")) {
+                kendoAdditional.RefreshGrid("#units");
+            }
+            if ($("#productionPlan")) {
+                kendoAdditional.RefreshGrid("#productionPlan");
+            }
+            if ($("#tanks")) {
+                kendoAdditional.RefreshGrid("#tanks");
+            }
             checkConfirmedStatus();
 
             var grid = $('#units').data('kendoGrid');
@@ -42,67 +25,83 @@ var unitGridsData = (function () {
 
         nameGridCommancolumn();
         hideCommandButtons();
-        AttachEventToExportBtn("#excel-export", "#units");
+        attachEventToExportBtn("#excel-export", "#units");
 
-        $("#confirm").click(function () {
-            var date = sendDate();
-            $.ajax({
-                url: 'Confirm',
-                type: 'POST',
-                data: date,
-                success: function (data) {
-                    var confirmed = data.IsConfirmed;
-                    if (confirmed === true) {
-                        hideCommandButtons();
-                        var message = "Вие потвърдихте отчета успешно."
-                        $('pre#succ-message').text(message);
-                        $('div#success-window').data("kendoWindow").open();
+        if ($("#confirm")) {
+            $("#confirm").click(function () {
+                var date = SendDate();
+                $.ajax({
+                    url: 'Confirm',
+                    type: 'POST',
+                    data: date,
+                    success: function (data) {
+                        var confirmed = data.IsConfirmed;
+                        if (confirmed === true) {
+                            hideCommandButtons();
+                            var message = "Вие потвърдихте отчета успешно."
+                            $('pre#succ-message').text(message);
+                            $('div#success-window').data("kendoWindow").open();
 
-                    } else {
-                        if (data.errors) {
-                            var errorMessage = "";
-                            $.each(data.errors, function (key, value) {
-                                if ('errors' in value) {
-                                    $.each(value.errors, function () {
-                                        errorMessage += this + "\n";
-                                    });
-                                }
-                            });
-                            $('pre#err-message').text(errorMessage);
-                            $('div#err-window').data("kendoWindow").open();
+                        } else {
+                            if (data.errors) {
+                                var errorMessage = "";
+                                $.each(data.errors, function (key, value) {
+                                    if ('errors' in value) {
+                                        $.each(value.errors, function () {
+                                            errorMessage += this + "\n";
+                                        });
+                                    }
+                                });
+                                $('pre#err-message').text(errorMessage);
+                                $('div#err-window').data("kendoWindow").open();
+                            }
+                            showCommandButtons();
                         }
-                        showCommandButtons();
+                    },
+                    error: function (data) {
+                        var errorMessage = "";
+                        var response = JSON.parse(data.responseText).data;
+                        $.each(response.errors, function (key, value) {
+                            errorMessage += this + "\n";
+                        });
+                        $('pre#err-message').text(errorMessage);
+                        $('div#err-window').data("kendoWindow").open();
                     }
-                },
-                error: function (data) {
-                    var errorMessage = "";
-                    var response = JSON.parse(data.responseText).data;
-                    $.each(response.errors, function (key, value) {
-                        errorMessage += this + "\n";
-                    });
-                    $('pre#err-message').text(errorMessage);
-                    $('div#err-window').data("kendoWindow").open();
-                }
+                });
             });
-        });
+        }
     });
 
-    // local
+    //------------------ private functions ------------------------------------
+
+    var sendProcessUnit = function () {
+        return { "processUnitId": $('input[name=processunits]').val() };
+    }
+
+    var sendShift = function sendShift() {
+        return { "shiftId": $('input[name=shifts]').val() }
+    }
+
+    var sendZoneId = function() {
+        return { "parkId": $('input[name=parks]').val() }
+    }
+
+    var sendShiftsOffset = function() {
+        return { "shiftMinutesOffset": $('input[name=shifts]').val() }
+    }
 
     var hideCommandButtons = function () {
         $("#confirm").hide();
         $("#units").data("kendoGrid").hideColumn('commands');
     }
 
-    // local
     var showCommandButtons = function () {
         $("#confirm").show();
         $("#units").data("kendoGrid").showColumn('commands');
     }
 
-    // local
     var checkConfirmedStatus = function () {
-        var date = sendDate();
+        var date = SendDate();
         $.ajax({
             url: 'IsConfirmed',
             type: 'POST',
@@ -121,15 +120,127 @@ var unitGridsData = (function () {
         });
     }
 
-    // local
-    var AttachEventToExportBtn = function (buttonSelector, targetSelector) {
+    var attachEventToExportBtn = function (buttonSelector, targetSelector) {
         $(buttonSelector).click(function () {
             $(targetSelector).data("kendoGrid").saveAsExcel();
         });
     }
 
-    // global
-    var dataBound = function () {
+    var nameGridCommancolumn = function () {
+        var grid = $("#units").data("kendoGrid");
+        if (grid) {
+            $.each(grid.columns, function (index, value) {
+                if (!this.field) {
+                    this.field = "commands";
+                }
+            });
+        }
+    }
+
+    var addAntiForgeryToken = function (data) {
+        data.__RequestVerificationToken = $('input[name=__RequestVerificationToken]').val();
+        return data;
+    };
+
+    var manualEntry = function (ev) {
+        ev.preventDefault();
+        ev.stopPropagation();
+        var culture = $('#culture').val();
+        $.ajax({
+            url: ev.data.url,
+            type: "POST",
+            content: document.body,
+            data: JSON.parse(JSON.stringify(addAntiForgeryToken(ev.data.data), replacer))
+        }).done(function (data) {
+            if (data.success === undefined) {
+                $("#modal-dialog-body").html(data);
+                prepareValidationScripts();
+                showRecordHistoriModal();
+            } else {
+                if (data.success === false) {
+                    manualEntryDoneErrorHandler(data.errors);
+                }
+            }
+        }).fail(function (data) {
+            var message = "Възникна грешка";
+            if (data) {
+                var status = data.status + 0;
+                if (400 <= status && status < 500) {
+                    message = "Не e намерена заявената страница";
+                }
+                if (status >= 500) {
+                    message = "Възникна грешка при обработка на вашата заявка";
+                }
+            }
+            showKendoError(message);
+        });
+    }
+
+    var replacer = function replacer(key, value) {
+
+        if (value instanceof Date) {
+            return value.toLocaleString(culture.value);
+        }
+
+        if (value === parseInt(value)) {
+            return value.toString().replace(' ', '');
+        }
+
+        if (value === parseFloat(value)) {
+            return value.toLocaleString(culture.value).replace(/\s/g, "");
+        }
+
+        return value;
+    }
+
+    var showKendoError = function (message)
+    {
+        $('pre#err-message').text(message);
+        $('div#err-window').data("kendoWindow").open();
+    }
+
+    var manualEntryDoneErrorHandler = function (errors) {
+        if (errors) {
+            var message = "";
+            $.each(errors, function (key, value) {
+                message += value.ErrorMessage + "\n";
+            });
+            $('pre#err-message').text(message);
+            $('div#err-window').data("kendoWindow").open();
+        }
+    }
+
+    var prepareValidationScripts = function () {
+        var form = $('#modal-dialog-body');
+        if (form.executed)
+            return;
+
+        form.removeData("validator");
+        $.validator.unobtrusive.parse(document);
+        form.executed = true;
+    }
+
+    //------------------ public functions ------------------------------------
+
+    var SendDate = function () {
+        var result = { "date": $('input[name=date]').val() };
+        $.extend(result, sendProcessUnit());
+        if ($('input[name=shifts]')) {
+            $.extend(result, sendShift());
+        }
+        $.extend(result, sendAntiForgery());
+        return result;
+    }
+
+    var SendTanksData = function () {
+        var result = { "date": $('input[name=date]').val() };
+        $.extend(result, sendZoneId());
+        $.extend(result, sendShiftsOffset());
+        $.extend(result, sendAntiForgery());
+        return result;
+    }
+
+    var DataBound = function () {
         var dataView = this.dataSource.view();
         var manualIndicator = $('#manualIndicator').val();
         var manualCalcumated = $('#manualCalcumated').val();
@@ -178,99 +289,19 @@ var unitGridsData = (function () {
         }
     }
 
-    // local
-    var nameGridCommancolumn = function () {
-        var grid = $("#units").data("kendoGrid");
-        if (grid) {
-            $.each(grid.columns, function (index, value) {
-                if (!this.field) {
-                    this.field = "commands";
-                }
-            });
-        }
-    }
-
-    // global
-    var dataSave = function (ev) {
+    var DataSave = function (ev) {
         if (ev.type === 'update') {
             kendoAdditional.RefreshGrid('#units');
             kendoAdditional.RefreshGrid('#productionPlan');
         }
     }
 
-    // local
-    var AddAntiForgeryToken = function (data) {
-        data.__RequestVerificationToken = $('input[name=__RequestVerificationToken]').val();
-        return data;
-    };
-
-    // local
-    var manualEntry = function (ev) {
-        ev.preventDefault();
-        ev.stopPropagation();
-        var culture = $('#culture').val();
-        $.ajax({
-            url: ev.data.url,
-            type: "POST",
-            content: document.body,
-            data: JSON.parse(JSON.stringify(AddAntiForgeryToken(ev.data.data), replacer))
-        }).done(function (data) {
-            if (data.success === undefined) {
-                $("#modal-dialog-body").html(data);
-                prepareValidationScripts();
-                showRecordHistoriModal();
-            } else {
-                if (data.success === false) {
-                    manual_error_handler(data.errors);
-                }
-            }
-        }).fail(function (data) {
-            var message = "Възникна грешка";
-            if (data) {
-                var status = data.status + 0;
-                if (400 <= status && status < 500) {
-                    message = "Не e намерена заявената страница";
-                }
-                if (status >= 500) {
-                    message = "Възникна грешка при обработка на вашата заявка";
-                }
-            }
-            showKendoError(message);
-        });
-    }
-
-    // local
-    var replacer = function replacer(key, value) {
-
-        if (value instanceof Date) {
-            return value.toLocaleString(culture.value);
-        }
-
-        if (value === parseInt(value)) {
-            return value.toString().replace(' ', '');
-        }
-
-        if (value === parseFloat(value)) {
-            return value.toLocaleString(culture.value).replace(/\s/g, "");
-        }
-
-        return value;
-    }
-
-    // global
-    var manualEntryFailure = function (data) {
+    var ManualEntryFailure = function (data) {
 
         showKendoError("Възникна грешка при обработка на вашата заявка");
     }
 
-    var showKendoError = function (message)
-    {
-        $('pre#err-message').text(message);
-        $('div#err-window').data("kendoWindow").open();
-    }
-
-    // global
-    var successCalculateManualEntry = function (data) {
+    var SuccessCalculateManualEntry = function (data) {
         if (data) {
             if (data == "success") {
                 hideRecordHistoriModal();
@@ -279,35 +310,12 @@ var unitGridsData = (function () {
         }
     }
 
-    // local
-    var manual_error_handler = function (errors) {
-        if (errors) {
-            var message = "";
-            $.each(errors, function (key, value) {
-                message += value.ErrorMessage + "\n";
-            });
-            $('pre#err-message').text(message);
-            $('div#err-window').data("kendoWindow").open();
-        }
-    }
-
-    // local
-    var prepareValidationScripts = function () {
-        var form = $('#modal-dialog-body');
-        if (form.executed)
-            return;
-
-        form.removeData("validator");
-        $.validator.unobtrusive.parse(document);
-        form.executed = true;
-    }
-
-
     return {
-        sendDate: sendDate,
-        dataBound: dataBound,
-        dataSave: dataSave,
-        manualEntryFailure: manualEntryFailure,
-        successCalculateManualEntry: successCalculateManualEntry,
+        SendDate: SendDate,
+        SendTanksData: SendTanksData,
+        DataBound: DataBound,
+        DataSave: DataSave,
+        ManualEntryFailure: ManualEntryFailure,
+        SuccessCalculateManualEntry: SuccessCalculateManualEntry,
     };
 })();
