8e32017fb2eeed39aee4acc31b887a0560a664ae
NikolayKostadinov <Kostadinov.Nikolay.D@gmail.com>
2015-12-21 15:47:26 +0200

    BugFix UnitDailyChart Date

--- Source/Web/CollectingProductionDataSystem.Web/Scripts/custom/renderDailyGraphic.js
+++ Source/Web/CollectingProductionDataSystem.Web/Scripts/custom/renderDailyGraphic.js
@@ -2,11 +2,12 @@
     $(document).ready(function () {
         $("#apply").click(function () {
             var value = $('input[name=processunits]').val() || $('input[name=processunitsD]').val();
+            var date = kendo.format("{0:u}", $('#date').data('kendoDatePicker').value());
             if (value) {
                 $.ajax({
                     url: '/DailyReporting/UnitsDaily/DailyChart',
                     method: 'GET',
-                    data: { 'processUnitId': value, 'date': $('input[name=date]').val()||Date().toLocaleString('bg') }
+                    data: { 'processUnitId': value, 'date': date || Date() }
                 }).done(function (data) {
                     $('#chart-wrapper').html(data);
                 });
