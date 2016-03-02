e05b406e62939b2443af731a31cbf9660ce32646
NikolayKostadinov <Kostadinov.Nikolay.D@gmail.com>
2016-03-02 15:41:47 +0200

    Fixed Menu Final

--- Source/Web/CollectingProductionDataSystem.Web/Scripts/custom/clearSeparators.js
+++ Source/Web/CollectingProductionDataSystem.Web/Scripts/custom/clearSeparators.js
@@ -5,6 +5,7 @@
         var included = 0;
         var divider;
         var firstDivider;
+        var previousIncluded = 0;
         Array.prototype.forEach.call(lis, function (li) {
 
             if ($(li).hasClass('divider')) {
@@ -12,13 +13,14 @@
                     $(divider).attr('style', 'display:none !important');
                 }
                 divider = li;
+                previousIncluded = included;
                 included = 0;
             } else {
                 if ($(li).find('a').length > 0) {
                     included += 1;
                 }
 
-                if (divider !== undefined && firstDivider === undefined && included > 0) {
+                if (divider !== undefined && firstDivider === undefined && included > 0 && previousIncluded === 0) {
                     firstDivider = divider;
                     $(firstDivider).addClass('divider-top');
                 }
