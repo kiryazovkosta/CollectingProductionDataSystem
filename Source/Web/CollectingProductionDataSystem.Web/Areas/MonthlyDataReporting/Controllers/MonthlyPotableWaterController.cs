18cf3675f8dd064137fa710d01f5a613228f60fc
NikolayKostadinov <Kostadinov.Nikolay.D@gmail.com>
2016-04-21 16:37:52 +0300

    Mothly Changes

--- Source/Web/CollectingProductionDataSystem.Web/Areas/MonthlyDataReporting/Controllers/MonthlyPotableWaterController.cs
+++ Source/Web/CollectingProductionDataSystem.Web/Areas/MonthlyDataReporting/Controllers/MonthlyPotableWaterController.cs
@@ -91,7 +91,6 @@
         }
 
         [HttpGet]
-        [SummaryReportFilter]
         public ActionResult MonthlyPotableWaterReport(DateTime? reportDate,  bool? isReport)
         {
             if (isReport != null)
@@ -329,7 +328,7 @@
         [ValidateAntiForgeryToken]
         public ActionResult Report(DateTime? date)
         {
-            return RedirectToAction("MonthlyPotableWaterReport", new { reportDate = date });
+            return RedirectToAction("MonthlyPotableWaterReport", new { reportDate = date, isReport = false });
         }
 
         /// <summary>
