814a1cc1f3c57fbf817368e52d7a503ac937a840
NikolayKostadinov <Kostadinov.Nikolay.D@gmail.com>
2016-05-25 09:32:00 +0300

    Creating PlanConfiguration Area

---
+++ Source/Web/CollectingProductionDataSystem.Web/Areas/PlanConfiguration/PlanConfigurationAreaRegistration.cs
@@ -0,0 +1,25 @@
+﻿using System.Web.Mvc;
+
+namespace CollectingProductionDataSystem.Web.Areas.PlanConfiguration
+{
+    public class PlanConfigurationAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "PlanConfiguration";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "PlanConfiguration_default",
+                "PlanConfiguration/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional },
+                namespaces: new string[] { "CollectingProductionDataSystem.Web.Areas.PlanConfiguration.Controllers"}
+            );
+        }
+    }
+}
