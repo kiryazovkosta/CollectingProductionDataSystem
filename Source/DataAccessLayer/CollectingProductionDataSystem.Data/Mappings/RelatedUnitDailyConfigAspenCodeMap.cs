187f38cde1797e749348162f59da3a3676d85acd
kiryazovkosta <kosta.kiryazov@gmail.com>
2016-06-09 16:32:02 +0300

    Added a UnitDailyConfig 2 Aspen Code table

---
+++ Source/DataAccessLayer/CollectingProductionDataSystem.Data/Mappings/RelatedUnitDailyConfigAspenCodeMap.cs
@@ -0,0 +1,38 @@
+﻿namespace CollectingProductionDataSystem.Data.Mappings
+{
+    using System;
+    using System.Data.Entity.Core.Common.CommandTrees.ExpressionBuilder;
+    using System.Data.Entity.ModelConfiguration;
+    using System.Linq;
+    using CollectingProductionDataSystem.Models.Productions.Daily;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Infrastructure.Annotations;
+
+    public class RelatedUnitDailyConfigAspenCodeMap : EntityTypeConfiguration<RelatedUnitDailyConfigAspenCode>
+    {
+        public RelatedUnitDailyConfigAspenCodeMap()
+        {
+            // Primary Key
+            this.HasKey(t => t.Id);
+
+             this.Property(u => u.Id)
+                .HasDatabaseGeneratedOption(DatabaseGeneratedOption.Identity);
+
+            this.Property(u => u.UnitDailyConfigId)
+                .HasColumnAnnotation("Index", new IndexAnnotation(new IndexAttribute("Ix_unit_daily_conf_aspen_code", 1)))
+                .IsRequired();
+
+            this.Property(u => u.AspenCode)
+                .HasColumnAnnotation("Index", new IndexAnnotation(new IndexAttribute("Ix_unit_daily_conf_aspen_code", 2)))
+                .IsRequired();
+
+            // Properties
+            this.Property(t => t.AspenCode)
+                .IsRequired()
+                .HasMaxLength(50);
+
+            // Table & Column Mappings
+            this.ToTable("UnitDailyConfigs2AspenCodes");
+        }
+    }
+}
---
+++ Source/Domain/CollectingProductionDataSystem.Models/Productions/Daily/RelatedUnitDailyConfigAspenCode.cs
@@ -0,0 +1,15 @@
+﻿namespace CollectingProductionDataSystem.Models.Productions.Daily
+{
+    using System;
+    using System.Linq;
+    using CollectingProductionDataSystem.Models.Contracts;
+
+    public partial class RelatedUnitDailyConfigAspenCode : IEntity
+    {
+        public int Id { get; set; }
+
+        public int UnitDailyConfigId { get; set; }
+
+        public string AspenCode { get; set; }
+    }
+}
