792e6769a20468acf7bc0906fa3e924e76ae840c
NikolayKostadinov <Kostadinov.Nikolay.D@gmail.com>
2016-06-14 10:32:12 +0300

    Bug Fix in AllAnual extention Method

--- Source/DataAccessLayer/CollectingProductionDataSystem.Data/Contracts/IDeletableEntityRepository.cs
+++ Source/DataAccessLayer/CollectingProductionDataSystem.Data/Contracts/IDeletableEntityRepository.cs
@@ -3,10 +3,10 @@
     using System.Linq;
     using CollectingProductionDataSystem.Contracts;
     using CollectingProductionDataSystem.Models.Contracts;
+    using CollectingProductionDataSystem.Models.Productions.Mounthly;
 
     public interface IDeletableEntityRepository<T> : IRepository<T> where T : IEntity
     {
         IQueryable<T> AllWithDeleted();
-
     }
 }
