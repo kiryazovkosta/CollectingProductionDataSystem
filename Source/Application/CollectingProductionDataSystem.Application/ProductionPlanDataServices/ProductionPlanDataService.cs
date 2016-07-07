cc450c576ca01396776c7e631f764cd1e35e97bc
kiryazovkosta <kosta.kiryazov@gmail.com>
2016-07-07 18:25:26 +0300

    Change mechanism of calculation of the monthly usage rate of production plan data

--- Source/Application/CollectingProductionDataSystem.Application/ProductionPlanDataServices/ProductionPlanDataService.cs
+++ Source/Application/CollectingProductionDataSystem.Application/ProductionPlanDataServices/ProductionPlanDataService.cs
@@ -120,6 +120,8 @@
 
                 var percentagesPlan = productionPlan.PlanNorms.Where(x => x.Month == month).First().Value;
 
+                var percentagesFactCurrentMonth = GetValidValueOrZero(CalculatePercentagesFactCurrentMonth(productionPlan, dailyData, this.calculator));
+
                 var productionPlanData = new ProductionPlanData
                 {
                     ProductionPlanConfigId = productionPlan.Id,
@@ -133,13 +135,14 @@
                     FactoryId = productionPlan.ProcessUnit.FactoryId,
                     ProcessUnitId = processUnitId.Value,
                     QuanityFactCurrentMonth = currentMonthQuantity.ContainsKey(productionPlan.Name) == true ?  currentMonthQuantity[productionPlan.Name] : 0.00m,
+                    PercentagesFactCurrentMonth = percentagesFactCurrentMonth
                 };
 
-                if (materialTypeId >= CommonConstants.EnergyType)
-                {
-                    var percs = CalculateUsageRateToTheDayValue(productionPlan, dailyData, this.calculator, productionPlanData);
-                    productionPlanData.PercentagesFactCurrentMonth = GetValidValueOrZero(percs);
-                }
+                //if (materialTypeId >= CommonConstants.EnergyType)
+                //{
+                //    var percs = CalculateUsageRateToTheDayValue(productionPlan, dailyData, this.calculator, productionPlanData);
+                //    productionPlanData.PercentagesFactCurrentMonth = GetValidValueOrZero(percs);
+                //}
 
                 if (productionPlan.Name == totallyQuantity.First().Key)
 	            {
@@ -168,16 +171,39 @@
                 totallyQuantityAs = totallyQuantity.First().Value;
             }
 
-            foreach (var item in result)
+            return result;
+        }
+
+        private double CalculatePercentagesFactCurrentMonth(ProductionPlanConfig productionPlan, List<UnitsDailyData> dailyData, ICalculatorService calculator)
+        {
+            var splitter = new char[] { '@' };
+
+            string[] planTokens = productionPlan.UsageRateMembers.Split(splitter, StringSplitOptions.RemoveEmptyEntries);
+            var planInputParamsValues = new List<double>();
+            foreach (var token in planTokens)
             {
-                if (materialTypeId == CommonConstants.MaterialType)
+                foreach (var item in dailyData)
                 {
-                    var percs = (((double)item.QuanityFactCurrentMonth + (double)item.QuantityFact) * 100.00) / (double)totallyQuantityAs;
-                    item.PercentagesFactCurrentMonth = GetValidValueOrZero(percs);
+                    if (item.UnitsDailyConfig.Code == token)
+                    {
+                        planInputParamsValues.Add((double) item.RealValueTillDay);
+                        break;
+                    }
                 }
             }
 
-            return result;
+            var planInputParams = new Dictionary<string, double>();
+            for (int i = 0; i < planInputParamsValues.Count(); i++)
+            {
+                planInputParams.Add(string.Format("p{0}", i), planInputParamsValues[i]);
+            }
+
+            var planValue = calculator.Calculate(productionPlan.UsageRateFormula, "p", planInputParams.Count, planInputParams, productionPlan.Code);
+            if (double.IsNaN(planValue) || double.IsInfinity(planValue))
+            {
+                planValue = 0.0;
+            }
+            return planValue;
         }
 
         private double CalculateUsageRateValue(ProductionPlanConfig productionPlan, List<UnitsDailyData> dailyData, ICalculatorService calculatorService)
