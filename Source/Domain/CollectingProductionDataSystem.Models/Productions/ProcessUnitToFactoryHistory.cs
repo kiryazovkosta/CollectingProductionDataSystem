c72a5877436f43e2ecb9a98726e738cda5e4d708
NikolayKostadinov <Kostadinov.Nikolay.D@gmail.com>
2017-02-14 10:03:56 +0200

    Added domain class for ProcessUnit to Factory relationship history

---
+++ Source/Domain/CollectingProductionDataSystem.Models/Productions/ProcessUnitToFactoryHistory.cs
@@ -0,0 +1,41 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="ProcessUnitToFactoryHistory.cs" company="Business Management System Ltd.">
+//     Copyright "2017" (c) Business Management System Ltd.. All rights reserved.
+// </copyright>
+// <author>Nikolay.Kostadinov</author>
+//-----------------------------------------------------------------------
+
+namespace CollectingProductionDataSystem.Models.Productions
+{
+    #region Usings
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Abstract;
+    using Contracts;
+
+    #endregion
+
+    /// <summary>
+    /// Summary description for ProcessUnitToFactoryHistory
+    /// </summary>
+    class ProcessUnitToFactoryHistory:DeletableEntity, IEntity
+    {
+        public int Id { get; set; }
+        public int ProcessUnitId { get; set; }
+        public virtual ProcessUnit ProcessUnit { get; set; }
+        public DateTime ActivationDate { get; set; }
+        public string ProcessUnitShortName { get; set; }
+        public string ProcessUnitFullName { get; set; }
+        public int FactoryId { get; set; }
+        public virtual Factory Factory { get; set; }
+        public string FactoryShortName { get; set; }
+        public string FactoryFullName { get; set; }
+        public int PlantId { get; set; }
+        public string PlantShortName { get; set; }
+        public string PlantFullName { get; set; }
+        public override string ToString() => $"{this.FactoryShortName} -> {this.ProcessUnitShortName} -> {this.ActivationDate}";
+    }
+}
